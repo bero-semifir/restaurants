38b0c70c8587d7fbec390709bcf7e383f03e1eae
Benoit <benoit.routier@semifir.com>
2021-05-27 13:47:47 +0200

    Ajout du Dockerfile

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 require('dotenv').config();
 require('colors');
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 const app = express();
 app.use(morgan('dev'));
